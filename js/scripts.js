7f6fe0824232a616e9ad044b0fb73d541f67c8c1
Clayton Collins and Michaela Davis <clayton@>
2017-01-26 16:27:52 -0800

    add JS business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,36 @@
 // BUSINESS LOGIC
 
+function User(userName, initialDeposit, deposit, withdrawal) {
+  this.userName = userName;
+  this.initialDeposit = initialDeposit;
+  this.deposit = deposit;
+  this.withdrawal = withdrawal;
+}
+
+User.prototype.balance = function() {
+  return ((this.initialDeposit + this.deposit) - this.withdrawal);
+}
+
+var customers = []
+
 // USER LOGIC
 $(document).ready(function() {
   $("form#daBank").submit(function(event) {
     event.preventDefault();
-    var userName = $(("#name-input").val());
+    var userName = $("#name-input").val();
     var initialDeposit = parseInt($("input#deposit-input").val());
     var deposit = parseInt($("input#deposit-amount").val());
-    var withdrawl = parseInt($("input#withdrawl-amount").val());
+    var withdrawal = parseInt($("input#withdraw-amount").val());
 
-    $("#balance").text(balance);
+    var user = new User(userName, initialDeposit, deposit, withdrawal);
+    customers.push(user);
 
+    $("#user-name").text(userName);
+    $("#balance").text(user.balance());
+    $(".customers").text("");
+    for (counter=0;counter<customers.length;counter++) {
+    $(".customers").append("<li>"+customers[counter].userName+"</li>");
+    console.log(customers);
+  };
   });
 });
